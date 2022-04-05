a57cbf4dc74789259c6105f08f3f3b3a7d1635d9
NitaLewska <Nita@ya.ru>
2022-04-06 01:10:39 +0300

    feat: add ability to add new points and to set new plan value

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
 var lineDiv = document.getElementById("line-chart");
 
 let time = []
-for(let i=0; i<=23; i++) {
-
+for(let i=0; i<=10; i++) {
+  for(let j=0; j<=59; j++) {
+    if (j<10) {
+      time.push("0"+i+":0"+j)  
+    }else{
+      time.push("0"+i+":"+j)
+    }
+  }
+}
+for(let i=10; i<=23; i++) {
   for(let j=0; j<=59; j++) {
     if (j<10) {
       time.push(""+i+":0"+j)  
@@ -12,7 +20,7 @@ for(let i=0; i<=23; i++) {
   }
 }
 
-var extraction_plan = {
+var extractionPlan = {
     y: [],
     x: time,
     name:"План добычи",
@@ -20,39 +28,36 @@ var extraction_plan = {
     type: 'scatter'
 }
 
-let target_rate = 100
+let targetRate = 100
 
 for (let i=0; i<time.length; i++) {
-  extraction_plan.y[i] = target_rate;
+  extractionPlan.y[i] = targetRate;
 } 
 
-var extraction_hour = {
+let extractionHour = {
   y: [0, 23, 0, 0, 30, 1, 30, 50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 89, 0, 0, 0],
   x: time,
   type: "bar",
   name:"Добыто (час)"
 };
+for (let i=0; i<time.length; i++) {
+  if (!extractionHour.y[i]) {
+    extractionHour.y[i] = 0
+  }
+} 
 
-var extraction_day = {
+let extractionDay = {
   x: time,
-  y: [extraction_hour.y[0]],
+  y: [extractionHour.y[0]],
   type: "scater",
   name:"Добыто (сутки)"
 };
 
-/* var extraction_plan = {
-    x: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
-    y: [extraction_hour.y[0]],
-    type: "scater",
-  };
-
-extraction_plan.y[25] = extraction_day.y.24/  */ 
-
-for (let i=1; i<=extraction_hour.x.length; i++) {
-    extraction_day.y.push(extraction_day.y[i-1]+extraction_hour.y[i])
+for (let i=1; i<=extractionHour.x.length; i++) {
+    extractionDay.y.push(extractionDay.y[i-1]+extractionHour.y[i])
 }
 
-var data = [extraction_hour, extraction_day, extraction_plan];
+var data = [extractionHour, extractionDay, extractionPlan];
 
 var layout = {
   title: "Скважина 1-1",
@@ -85,3 +90,31 @@ var layout = {
 var config = { responsive: true };
 
 Plotly.plot(lineDiv, data, layout, config, {scrollZoom: true});
+
+
+/* Ввод данных */
+
+btnPlan = document.querySelector("#btn_plan")
+btnPlan.addEventListener('click', function(e) {
+  e.preventDefault()
+  targetRate = document.querySelector("#input_plan").value
+  for (let i=0; i<time.length; i++) {
+    extractionPlan.y[i] = targetRate;
+  } 
+  Plotly.redraw(lineDiv)
+})
+
+btnAdd = document.querySelector("#btn_add")
+btnAdd.addEventListener('click', function(e) {
+  e.preventDefault()
+  let xNew = time.indexOf(document.querySelector('#time_new').value)
+  let yNew = document.querySelector("#extracted_new").value
+  extractionHour.y[xNew] = yNew;
+  for (let i=1; i<=extractionHour.x.length; i++) {
+    extractionDay.y[i]=(parseInt(extractionDay.y[i-1])+parseInt(extractionHour.y[i]))
+  }
+  Plotly.redraw(lineDiv)
+document.querySelector('#time_new').value = 0
+document.querySelector("#extracted_new").value = 0
+
+})
